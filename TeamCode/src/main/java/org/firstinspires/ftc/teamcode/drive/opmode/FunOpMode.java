b930a0f659c6bcb0971f4edc538b834775237bcb
Anderson W <anderson.westfall@student.tamdistrict.org>
2023-08-04 14:23:28 -0700

    star and trajectory

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/opmode/FunOpMode.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/opmode/FunOpMode.java
@@ -4,7 +4,9 @@ import com.acmerobotics.dashboard.FtcDashboard;
 import com.acmerobotics.dashboard.config.Config;
 import com.acmerobotics.dashboard.telemetry.MultipleTelemetry;
 import com.acmerobotics.roadrunner.geometry.Pose2d;
+import com.acmerobotics.roadrunner.geometry.Vector2d;
 import com.acmerobotics.roadrunner.trajectory.Trajectory;
+import com.acmerobotics.roadrunner.trajectory.TrajectoryBuilder;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 
@@ -25,16 +27,37 @@ public class FunOpMode extends LinearOpMode {
 
         SampleMecanumDrive drive = new SampleMecanumDrive(hardwareMap);
 
-        Trajectory trajectory = drive.trajectoryBuilder(new Pose2d())
-                .lineToLinearHeading(new Pose2d(60.0, 0.0, Math.toRadians(90)))
-                .build();
+//        Trajectory forwardRightTrajectory = drive.trajectoryBuilder(new Pose2d())
+//                .lineToLinearHeading(new Pose2d(60.0, 0.0, Math.toRadians(-90)))
+//                .build();
+
+        Pose2d[] targetPositions = {
+                // Square
+//                new Pose2d(60, 0, Math.toRadians(-90)),
+//                new Pose2d(60, -60, Math.toRadians(180)),
+//                new Pose2d(0, -60, Math.toRadians(90)),
+//                new Pose2d(0, 0, Math.toRadians(0)),
+                // Star
+                new Pose2d(0, -60, Math.toRadians(180)),
+                new Pose2d(-35.26, -11.46, Math.toRadians(360)),
+                new Pose2d(18, -30, Math.toRadians(180)),
+                new Pose2d(-36.26, -48.54, Math.toRadians(360)),
+                new Pose2d(0,0, Math.toRadians(180)),
+        };
 
         waitForStart();
 
         if (isStopRequested()) return;
 
-        drive.followTrajectory(trajectory);
+        for(int i = 0; i < targetPositions.length; i++){
+            Pose2d pose = targetPositions[i];
+            Trajectory trajectory = drive.trajectoryBuilder(drive.getPoseEstimate())
+                    .lineToLinearHeading(pose)
+                    .build();
+            drive.followTrajectory(trajectory);
+        }
 
+        // Final telemetry
         Pose2d poseEstimate = drive.getPoseEstimate();
         telemetry.addData("finalX", poseEstimate.getX());
         telemetry.addData("finalY", poseEstimate.getY());
